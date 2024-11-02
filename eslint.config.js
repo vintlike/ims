7e01312ee076759f608f7e0ae0d2a77cd4e92b0e
vintlike <vintlike@163.com>
2024-11-03 00:34:46 +0800

    feat: 增加项目配置文件

--- eslint.config.js
+++ eslint.config.js
@@ -1,28 +1,44 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
-  { ignores: ['dist'] },
+  { ignores: ['dist', 'node_modules'] },
   {
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
-      globals: globals.browser,
+      globals: globals.browser
     },
     plugins: {
       'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
+      'react-refresh': reactRefresh
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
       'react-refresh/only-export-components': [
         'warn',
-        { allowConstantExport: true },
+        { allowConstantExport: true }
       ],
-    },
-  },
-)
+      '@typescript-eslint/consistent-type-imports': ['warn'],
+      // 关闭定义变量未使用提示/报错
+      '@typescript-eslint/no-unused-vars': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+      'prefer-spread': ['off'],
+      '@typescript-eslint/no-empty-interface': ['off'],
+      'no-var': 'off',
+      '@typescript-eslint/no-inferrable-types': 'off',
+      '@typescript-eslint/no-var-requires': 'off',
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-empty-function': 'off',
+      'no-constant-condition': 'warn',
+      'no-unsafe-finally': 'off',
+      '@typescript-eslint/no-non-null-assertion': 'off',
+      'prefer-const': 'off',
+      'ts/no-unused-vars': 'off'
+    }
+  }
+);
